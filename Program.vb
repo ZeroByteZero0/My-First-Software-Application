bbdb56e9eead5bc96ddeec73648cf3f6ad4afbe3
MJ <146496042+ZeroByteZero0@users.noreply.github.com>
2023-11-27 17:08:37 -0800

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Module1
+    Sub Main(args As String())
+        REM First Program
+        'another comment
+        Console.WriteLine("My first program")
+        Console.ReadKey()
+    End Sub
+End Module
